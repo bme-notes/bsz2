53bfc701a0b680b0d00b34d0b3ac562dad6437c0
vista- <hegyi.mokka@gmail.com>
2015-05-24 18:56:57 +0200

    add full build script, small fix in 12, add makelog to .gitignore

---
+++ make_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm makelog
+echo "generating separate theses..."
+for i in *_tetel.tex; do
+	echo -n "${i}..."
+	echo "===processing ${i}===" >> makelog
+	xelatex -8bit --shell-escape -synctex=1 --output-directory=pdfs/ -interaction=nonstopmode "${i}" >> makelog
+	ret=$?
+	if [ $ret -eq 0 ]; then
+	 	echo "OK"
+	else
+		echo "[!] Failed, check ${i%.tex}.log"
+	fi
+	echo "===end of ${i}===" >> makelog
+done
+source create_stripped.sh
+echo -n "generating complete document..."
+echo "===processing full_document.tex===" >> makelog
+xelatex -8bit --shell-escape -synctex=1 --output-directory=pdfs/ -interaction=nonstopmode "full_document".tex >> makelog
+echo "===end of full_document.tex===" >> makelog
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo "OK"
+else
+	echo "[!] Failed, check full_document.log"
+fi
+echo "cleaning up output directory"
+rm ./pdfs/*.aux
+rm ./pdfs/*.log
+rm ./pdfs/*.gz
+rm ./pdfs/*.out
